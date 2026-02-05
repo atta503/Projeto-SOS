69752ec0be6c19cd8c2b8694b93caf43fa3bde03
Henrique Pena <penahenrique.3@gmail.com>
2026-02-05 13:42:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,54 +3,34 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
+void pisca(int tempo){
+  gpio_set_level(2,1);
+    vTaskDelay(tempo / portTICK_PERIOD_MS);
+    gpio_set_level(2,0);
+    vTaskDelay(250 / portTICK_PERIOD_MS);
+}
+void S(){
+  pisca(200);
+    pisca(200);
+    pisca(200);
+    printf("S\n"); //Primeiro carácter
+}
+void O(){
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    pisca(800);
+    pisca(800);
+    pisca(800);
+    printf("O\n"); //Segundo carácter
+}
 void app_main() {
   gpio_set_direction(2,GPIO_MODE_OUTPUT);
   gpio_set_level(2,0);
   while (true) {
-    vTaskDelay(250 / portTICK_PERIOD_MS);
-    gpio_set_level(2,0);
-     vTaskDelay(250 / portTICK_PERIOD_MS);
-    gpio_set_level(2,1);
-    vTaskDelay(250 / portTICK_PERIOD_MS);
-    gpio_set_level(2,0);
-    vTaskDelay(250 / portTICK_PERIOD_MS);
-      gpio_set_level(2,1);
-      vTaskDelay(250 / portTICK_PERIOD_MS);
-      gpio_set_level(2,0);
-      vTaskDelay(250 / portTICK_PERIOD_MS);
-      gpio_set_level(2,1);
-      vTaskDelay(250 / portTICK_PERIOD_MS);
-       gpio_set_level(2,0);
-       printf("S\n"); //Primeiro carácter
-       vTaskDelay(500 / portTICK_PERIOD_MS);
-       gpio_set_level(2,1);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-         gpio_set_level(2,0);
-         vTaskDelay(500 / portTICK_PERIOD_MS);
-         gpio_set_level(2,1);
-         vTaskDelay(500 / portTICK_PERIOD_MS);
-         gpio_set_level(2,0);
-         vTaskDelay(500 / portTICK_PERIOD_MS);
-         gpio_set_level(2,1);
-         vTaskDelay(500 / portTICK_PERIOD_MS);
-         gpio_set_level(2,0);
-         printf("O\n"); //Segundo carácter
-         vTaskDelay(250 / portTICK_PERIOD_MS);
-         gpio_set_level(2,1);
-          vTaskDelay(250 / portTICK_PERIOD_MS);
-           gpio_set_level(2,0);
-            vTaskDelay(250 / portTICK_PERIOD_MS);
-            gpio_set_level(2,1);
-             vTaskDelay(250 / portTICK_PERIOD_MS);
-             gpio_set_level(2,0);
-              vTaskDelay(250 / portTICK_PERIOD_MS);
-              gpio_set_level(2,1);
-              vTaskDelay(250 / portTICK_PERIOD_MS);
-              gpio_set_level(2,0);
-              printf("S\n"); //Terceiro carácter
-              vTaskDelay(1000 / portTICK_PERIOD_MS);
-              printf(" \n"); //Espaço para repetir o programa
-    
+  S();
+  O();
+  S();
+   vTaskDelay(250 / portTICK_PERIOD_MS);
+    printf("  \n"); //Primeiro carácter
   }
     
 }
